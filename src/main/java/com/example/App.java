7d1af55ba1ba51db5940cd93ec80dcac45f4e515
lai2-01 <lai2-01@itismeucci.com>
2023-10-09 10:19:59 +0200

    primo

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class App {
+    public static void main( String[] args ){
+        try {
+            Socket s = new Socket("localhost", 3000);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+            out.writeBytes("ciao a tutti \n");
+            String r = in.readLine();
+
+            System.out.println("Il server ha risporso:" + r);
+            s.close();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+}
